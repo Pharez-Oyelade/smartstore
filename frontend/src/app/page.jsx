32db34b1f162a5b9d03224c9a17a79f1ad22cf95
Pharez <pharezoyelade@gmail.com>
2025-12-28 21:35:56 +0100

    Landing page hero

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,7 +1,16 @@
+// landing page
+
 import React from "react";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
 };
 
 export default page;
